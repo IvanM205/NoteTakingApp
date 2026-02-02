d18f43061f0977c31ecc83517364837c53435d9c
Ivan Marianek <123243353+IvanM205@users.noreply.github.com>
2026-02-02 13:06:12 +0100

    prepared for deployement

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,18 +3,21 @@ const notesRoutes = require('./routes/notesRoutes');
 const { connectDB } = require('./config/db');
 const { rateLimiter } = require('./middleware/rateLimiter');
 const dotenv = require('dotenv');
+const path = require("path");
 const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
+const baseDir = process.cwd();
 
 // middleware
-app.use(cors({
-    origin: "http://localhost:5173",
-}));
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }));
+}
 app.use(express.json());
 app.use(rateLimiter);
 // app.use((req, res, next) => {
@@ -23,6 +26,14 @@ app.use(rateLimiter);
 // })
 
 app.use('/api/notes', notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(baseDir, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 
 connectDB().then(() => {
